ffa82fb6412f62bcd9992876f0b5413b77580788
Vatsal Saxena <vatsalsaxena22@gmail.com>
2025-02-18 17:44:41 +0530

    Build the Web Structure

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// Components
+import Navbar from './components/navbar'
+import Footer from './components/footer';
+// Pages
+import Home from './pages/home';
+import About from './pages/about';
+import Services from './pages/services';
+import Contact from './pages/contact';
+import NotFound from './pages/NotFound';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+        <Navbar/>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/services' element={<Services/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer/>
+    </BrowserRouter>
+  )
+}
+
+export default App
